0943d2f0f28827a4e0f513ce88ddf4d65ad656e3
Wesley Bliss <wes@fueled.com>
2013-01-26 15:51:17 -0500

    Fixed manifest permissions and generated a nested tree list of all bookmarks.

--- tagmyfavorites.js
+++ tagmyfavorites.js
@@ -2,9 +2,41 @@
 var TMF = {
     
     init: function() {
+        
         var debug = document.createElement('h1');
-        debug.innerHTML = 'testing';
+        debug.innerHTML = 'tag my favorites, v1';
         document.body.appendChild( debug );
+        
+        chrome.bookmarks.getTree( function(BookmarkTreeNodes) {
+            
+            var debug = document.createElement('h1');
+            debug.innerHTML = 'found ' + BookmarkTreeNodes[0].children.length.toString() + ' favs.';
+            document.body.appendChild( debug );
+            
+            console.log( BookmarkTreeNodes );
+            
+            var ul = document.createElement('ul');
+            TMF.showBookmarksRecursively( BookmarkTreeNodes[0].children, ul );
+            document.body.appendChild( ul );
+            
+        });
+        
+    },
+    
+    showBookmarksRecursively: function( bookmarkTreeNodes, parentElement ) {
+        for ( var i = 0; i < bookmarkTreeNodes.length; i++ ) {
+            console.log( bookmarkTreeNodes[i] );
+            if ( bookmarkTreeNodes[i].title ) {
+                var li = document.createElement('li');
+                li.innerHTML = bookmarkTreeNodes[i].title;
+                parentElement.appendChild( li );
+                if ( bookmarkTreeNodes[i].children ) {
+                    var subParent = document.createElement('ul');
+                    TMF.showBookmarksRecursively( bookmarkTreeNodes[i].children, subParent );
+                    li.appendChild( subParent );
+                }
+            }
+        }
     },
     
     requestKittens: function() {
@@ -20,4 +52,10 @@ var TMF = {
 //
 document.addEventListener( 'DOMContentLoaded', function() {
     TMF.init();
+    
+    document.getElementById('go').onclick = function(e) {
+        e.preventDefault();
+        TMF.init();
+    };
+    
 });
